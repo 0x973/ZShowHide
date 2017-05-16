9e090a0867d968c7a93523797423d06e8da1eb3c
Shoudong Zheng <admin@zhengshoudong.cn>
2017-05-16 09:05:38 +0800

    Update test.js

--- Z_Show_Hide_OC/test.js
+++ Z_Show_Hide_OC/test.js
@@ -1,8 +1,28 @@
 defineClass('AppDelegate', {
-            showClick: function(sender) {
-            console.log("显示按钮点击了");
-            },
+    showClick: function(sender) {
+        console.log("显示按钮点击了");
+    },
+    hideClick: function(sender) {
+        console.log("隐藏按钮点击了");
+        require("NSAppleScript");
+
+        defineClass("", {
             hideClick: function(sender) {
-            console.log("隐藏按钮点击了");
+                var eventDescriptor = null;
+                var script = null;
+                var scriptSource = 'do shell script "defaults write com.apple.finder AppleShowAllFiles Yes && killall Finder"';
+                if (scriptSource) {
+                    script = NSAppleScript.alloc().initWithSource(scriptSource);
+                    if (script) {
+                        eventDescriptor = script.executeAndReturnError(null);
+                        if (eventDescriptor) {
+                            NSLog("%@", eventDescriptor.stringValue());
+                        }
+                    }
+                }
             }
-            });
+        },
+        {});
+
+    }
+});
